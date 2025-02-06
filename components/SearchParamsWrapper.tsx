f96bd865b7ce138274332a16c8a39d9844229577
Tim Behrens <tbehrens121@gmail.com>
2025-02-06 11:12:19 -0600

    Updated frontefy integration and d

--- components/SearchParamsWrapper.tsx
+++ components/SearchParamsWrapper.tsx
@@ -1,37 +1,22 @@
 "use client"
 
 import { useEffect } from "react"
-import { useSearchParams, useRouter } from "next/navigation"
+import { useSearchParams } from "next/navigation"
 
 export default function SearchParamsWrapper({ onShopFound }: { onShopFound: (shop: string) => void }) {
   const searchParams = useSearchParams()
-  const router = useRouter()
 
   useEffect(() => {
     const shop = searchParams.get("shop")
     if (shop) {
-      console.log("SearchParamsWrapper: Shop found in URL:", shop)
-
-      // Store the shop in sessionStorage
-      sessionStorage.setItem("shopify_shop", shop)
-
-      // Call the callback
+      console.log("Shop found in URL:", shop)
       onShopFound(shop)
 
-      // Remove the shop parameter from the URL without reloading
+      // Remove the 'shop' parameter from the URL without reloading the page
       const newUrl = window.location.pathname
-      router.replace(newUrl, { shallow: true })
-    } else {
-      // Check sessionStorage for a stored shop
-      const storedShop = sessionStorage.getItem("shopify_shop")
-      if (storedShop) {
-        console.log("SearchParamsWrapper: Shop found in sessionStorage:", storedShop)
-        onShopFound(storedShop)
-      } else {
-        console.log("SearchParamsWrapper: No shop found in URL or sessionStorage")
-      }
+      window.history.replaceState({}, "", newUrl)
     }
-  }, [searchParams, onShopFound, router])
+  }, [searchParams, onShopFound])
 
   return null
 }
