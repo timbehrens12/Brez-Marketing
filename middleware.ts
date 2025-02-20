4365d410b4d12b7bcc2470b273dc2bbcb64d4148
Tim Behrens <tbehrens121@gmail.com>
2025-02-19 19:53:36 -0600

    working 19thcdcvcdwfds

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs"
 
 export default authMiddleware({
-  publicRoutes: ["/"]
+  publicRoutes: ["/", "/sign-in"],
+  afterAuth(auth, req) {
+    // Redirect to sign in if not authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      const signInUrl = new URL('/sign-in', req.url)
+      return Response.redirect(signInUrl)
+    }
+  }
 })
 
 export const config = {
