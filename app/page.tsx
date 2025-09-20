fc82eadd89bd41577d9a4f3a0b9498d4575ced4f
Tim Behrens <tbehrens121@gmail.com>
2025-09-19 22:26:34 -0500

    FIty2fhN3tg2322322t3hL3gwJuey3yw4wymbmjm34myw3y4hny37nnmy3yny33mmnnyy3m4n3nmyy33u4423n4m22mnYy3U622yy3ny3n63njn37m454n 473bn24yy4m3nU44MMNyu33

--- app/page.tsx
+++ app/page.tsx
@@ -313,7 +313,7 @@ export default function HomePage() {
              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                  <img src="https://i.imgur.com/j4AQPxj.png" alt="Brez Marketing" className="h-10 w-auto" />
              </div>
-              <div className="absolute right-12 sm:right-16 lg:right-20 top-1/2 -translate-y-1/2 flex items-center gap-3">
+              <div className="absolute right-10 sm:right-13 lg:right-16 top-1/2 -translate-y-1/2 flex items-center gap-3">
                <Link href="/dashboard">
                  <Button className="bg-transparent border border-white/20 hover:bg-white/10 text-white">
                    Go to Dashboard <ArrowRight className="ml-2 h-4 w-4" />
