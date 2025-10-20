5fd0b9ecbb21c10d68ed617e844eae5d5c354aca
Tim Behrens <tbehrens121@gmail.com>
2025-10-20 14:05:47 -0500

    Remove AuthGuard from dashboard layout to fix Clerk dependency issue

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,15 +1,9 @@
 "use client"
 
-import { AuthGuard } from "@/components/AuthGuard"
-
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <AuthGuard>
-      {children}
-    </AuthGuard>
-  )
+  return <>{children}</>
 }
