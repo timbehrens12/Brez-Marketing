9751fa1aa88aa2b35135f6478241a000e2ad5cb7
Tim Behrens <tbehrens121@gmail.com>
2025-10-23 03:40:46 -0500

    Fix Sidebar import - use named export instead of default export

--- components/AuthenticatedLayout.tsx
+++ components/AuthenticatedLayout.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { usePathname } from 'next/navigation'
-import Sidebar from './Sidebar'
+import { Sidebar } from './Sidebar'
 import { SidebarProvider } from '@/context/SidebarContext'
 
 // Routes that should NOT show the sidebar
