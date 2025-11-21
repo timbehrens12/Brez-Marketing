d187fbe2732d548d3941a54e70ff945249ebb708
Tim Behrens <tbehrens121@gmail.com>
2025-11-20 23:51:44 -0600

    Keep real wheel images but simplify all text to generic placeholder content

--- OneDrive/Desktop/WHEEL VIZ 2/components/sidebar/WheelSelector.tsx
+++ OneDrive/Desktop/WHEEL VIZ 2/components/sidebar/WheelSelector.tsx
@@ -8,119 +8,119 @@ import { Button } from '@/components/ui/button';
 import { generateImageAction } from '@/actions/generate';
 import { cn } from '@/lib/utils';
 
-// Realistic Fitment Industries wheel data
+// Wheel gallery with real images but generic placeholder text
 const FITMENT_WHEELS = [
   {
     id: 'w1',
     type: 'wheel',
-    brand: 'Volk Racing',
-    model: 'CE28N',
-    finish: 'Hyper Black',
+    brand: 'TITLE HERE',
+    model: 'MODEL HERE',
+    finish: 'FINISH HERE',
     diameter: 18,
     width: 9.5,
     offset: 35,
-    boltPattern: '5x114.3',
+    boltPattern: 'BOLT PATTERN HERE',
     price: 1295,
     imageUrl: 'https://i.imgur.com/7b7S0bn.png',
-    productUrl: 'https://fitmentindustries.com/products/volk-racing-ce28n-18x9-5-35-hyper-black'
+    productUrl: 'https://fitmentindustries.com/products/wheel-1'
   },
   {
     id: 'w2',
     type: 'wheel',
-    brand: 'BBS',
-    model: 'FI-R',
-    finish: 'Diamond Black',
+    brand: 'TITLE HERE',
+    model: 'MODEL HERE',
+    finish: 'FINISH HERE',
     diameter: 19,
     width: 9.5,
     offset: 22,
-    boltPattern: '5x114.3',
+    boltPattern: 'BOLT PATTERN HERE',
     price: 1595,
     imageUrl: 'https://i.imgur.com/o6LRJZr.png',
-    productUrl: 'https://fitmentindustries.com/products/bbs-fi-r-19x9-5-22-diamond-black'
+    productUrl: 'https://fitmentindustries.com/products/wheel-2'
   },
   {
     id: 'w3',
     type: 'wheel',
-    brand: 'Work Equip',
-    model: '03',
-    finish: 'Bronze',
+    brand: 'TITLE HERE',
+    model: 'MODEL HERE',
+    finish: 'FINISH HERE',
     diameter: 18,
     width: 10.5,
     offset: 25,
-    boltPattern: '5x114.3',
+    boltPattern: 'BOLT PATTERN HERE',
     price: 1895,
     imageUrl: 'https://i.imgur.com/LKnghBP.png',
-    productUrl: 'https://fitmentindustries.com/products/work-equip-03-18x10-5-25-bronze'
+    productUrl: 'https://fitmentindustries.com/products/wheel-3'
   },
   {
     id: 'w4',
     type: 'wheel',
-    brand: 'HRE',
-    model: 'P1',
-    finish: 'Flow Form Silver',
+    brand: 'TITLE HERE',
+    model: 'MODEL HERE',
+    finish: 'FINISH HERE',
     diameter: 20,
     width: 10,
     offset: 30,
-    boltPattern: '5x114.3',
+    boltPattern: 'BOLT PATTERN HERE',
     price: 2195,
     imageUrl: 'https://i.imgur.com/LoJwUdw.png',
-    productUrl: 'https://fitmentindustries.com/products/hre-p1-20x10-5-30-flow-form-silver'
+    productUrl: 'https://fitmentindustries.com/products/wheel-4'
   },
   {
     id: 'w5',
     type: 'wheel',
-    brand: 'Rotiform',
-    model: 'BLQ',
-    finish: 'Matte Bronze',
+    brand: 'TITLE HERE',
+    model: 'MODEL HERE',
+    finish: 'FINISH HERE',
     diameter: 19,
     width: 9.5,
     offset: 20,
-    boltPattern: '5x114.3',
+    boltPattern: 'BOLT PATTERN HERE',
     price: 1795,
     imageUrl: 'https://i.imgur.com/Y7d2ox8.png',
-    productUrl: 'https://fitmentindustries.com/products/rotiform-blq-19x9-5-20-matte-bronze'
+    productUrl: 'https://fitmentindustries.com/products/wheel-5'
   },
   {
     id: 'w6',
     type: 'wheel',
-    brand: 'Avant Garde',
-    model: 'M590',
-    finish: 'Diamond Cut Black',
+    brand: 'TITLE HERE',
+    model: 'MODEL HERE',
+    finish: 'FINISH HERE',
     diameter: 19,
     width: 9.5,
     offset: 25,
-    boltPattern: '5x114.3',
+    boltPattern: 'BOLT PATTERN HERE',
     price: 1995,
     imageUrl: 'https://i.imgur.com/bAtSVRV.png',
-    productUrl: 'https://fitmentindustries.com/products/avant-garde-m590-19x9-5-25-diamond-cut-black'
+    productUrl: 'https://fitmentindustries.com/products/wheel-6'
   },
   {
     id: 'w7',
     type: 'wheel',
-    brand: 'Enkei',
-    model: 'TS-10',
-    finish: 'Matte Gunmetal',
+    brand: 'TITLE HERE',
+    model: 'MODEL HERE',
+    finish: 'FINISH HERE',
     diameter: 18,
     width: 9.5,
     offset: 22,
-    boltPattern: '5x114.3',
+    boltPattern: 'BOLT PATTERN HERE',
     price: 1395,
     imageUrl: 'https://i.imgur.com/7XWHhJ6.png',
-    productUrl: 'https://fitmentindustries.com/products/enkei-ts-10-18x9-5-22-matte-gunmetal'
+    productUrl: 'https://fitmentindustries.com/products/wheel-7'
   },
   {
     id: 'w8',
     type: 'wheel',
-    brand: 'Forgestar',
-    model: 'F14',
-    finish: 'Gloss Black',
+    brand: 'TITLE HERE',
+    model: 'MODEL HERE',
+    finish: 'FINISH HERE',
     diameter: 18,
     width: 9,
     offset: 35,
-    boltPattern: '5x114.3',
+    boltPattern: 'BOLT PATTERN HERE',
     price: 1695,
     imageUrl: 'https://i.imgur.com/0bir2Rr.png',
-    productUrl: 'https://fitmentindustries.com/products/forgestar-f14-18x9-5-35-gloss-black'
+    productUrl: 'https://fitmentindustries.com/products/wheel-8'
   }
 ];
 
@@ -203,16 +203,16 @@ export const WheelSelector = ({ onProductSelect }: WheelSelectorProps = {}) => {
             <img src={wheel.imageUrl} alt={wheel.model} className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500" />
             <div className="absolute inset-0 bg-black/0 group-hover:bg-black/20 transition-colors duration-300" />
             <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/90 to-transparent p-3 pt-8">
-              <p className="text-xs font-bold text-white">{wheel.brand}</p>
-              <p className="text-[10px] text-neutral-300 truncate">{wheel.model}</p>
-              <p className="text-[10px] text-emerald-400 font-semibold">${wheel.price}</p>
+              <p className="text-xs font-bold text-white">TITLE HERE</p>
+              <p className="text-[10px] text-neutral-300 truncate">MODEL HERE</p>
+              <p className="text-[10px] text-emerald-400 font-semibold">$PRICE HERE</p>
             </div>
           </div>
           <div className="p-3 space-y-2">
             <div className="text-xs text-neutral-400 space-y-1">
-              <div>Size: {wheel.diameter}" x {wheel.width}"</div>
-              <div>Offset: {wheel.offset}mm</div>
-              <div>Bolt: {wheel.boltPattern}</div>
+              <div>DETAIL HERE</div>
+              <div>SPEC HERE</div>
+              <div>INFO HERE</div>
             </div>
             <div className="flex gap-2">
               <Button
